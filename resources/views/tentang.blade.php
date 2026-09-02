962db57857012d65ca0cfdd3a6f77e8b143afe59
Devin <devinradityapratama@gmail.com>
2026-09-02 23:33:50 +0800

    feat: Menambahkan route baru yaitu tentang

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Tentang Kelompok</title>
+</head>
+<body>
+
+    <h1>KampusLMS Kelompok 03</h1>
+
+    <h2>Anggota Kelompok</h2>
+
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Devin Raditya P</td>
+            <td>10241021</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Dewi Bulan Purnama</td>
+            <td>10241023</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Eagan Ferdian</td>
+            <td>10241025</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Fabyo Nathanael Suoth</td>
+            <td>10241027</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Fariz Daffa Abbiyu Rahmatullah</td>
+            <td>10241029</td>
+        </tr>
+    </table>
+
+</body>
+</html>
